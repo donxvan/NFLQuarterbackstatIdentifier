d9c5d652d1466b6a3ddf3404f863fd3bebf09f46
noberkof <51204269+noberkof@users.noreply.github.com>
2019-07-19 20:32:16 -0400

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,11 @@
 #this is our main code
+average_yards = 4613
+
+passing_yards = input("Enter an amount of passing yards for the 2017-2018 NFL Season.")
+
+if int(passing_yards) >= average_yards:
+    print("Ben Roethlisberger 5,129 yrds", "Patrick Mahomes 5,097 yrds", "Matt Ryan 4,924 yrds", "Jared Goff 4,688 yrds")
+elif int(passing_yards) <= average_yards:
+    print("Andrew Luck 4,493 yrds", "Aaron Rodgers 4,442 yrds", "Tom Brady 4,395 yrds", "Phillip Rivers 4,308 yrds", "Eli Manning 4,299 yrds", "Kirk Cousins 4,298 yrds") 
+else:
+    print("Try Again")
